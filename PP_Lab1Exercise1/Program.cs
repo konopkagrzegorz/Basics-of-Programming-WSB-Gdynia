fea34de45c6529def938bf2d9a012c6a1eadc6b8
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-26 22:35:37 +0100

    addinh tasks

---
+++ PP_Lab1Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab1Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howMany = 4;
+            double pricePerPiece = 3.51;
+
+            Console.WriteLine("Price is: " + (howMany * pricePerPiece));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PP_Lab1Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab1Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = 8;
+            double length = 12.5;
+            double height = 2.5;
+
+            Console.WriteLine("Do pomalowania scian potrzeba " + ((2 * length + 2 * width) * height) + " metrow kwadratowych farby.");
+            Console.ReadKey();
+        }
+    }
+}
