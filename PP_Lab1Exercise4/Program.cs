c62d6bf688e3a8b6b6626455bd8e5a12d3ca71bf
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-27 18:53:00 +0100

    added exercise 4

---
+++ PP_Lab1Exercise4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab1Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 7;
+            int minutes = 50;
+
+            if (hour > 8 || (hour == 8 && minutes > 20))
+            {
+                Console.WriteLine("O nie! Jesteś spózniony");
+            } else
+            {
+                Console.WriteLine("OK! Nie jestes spozniony");
+            }
+            Console.ReadKey();
+        }
+    }
+}
