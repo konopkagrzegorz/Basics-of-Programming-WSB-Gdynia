c3eb89f9caa7afc7b8c1e6e0b0259e93068ae603
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-28 22:52:15 +0100

    added exercise 2 from lab 2

---
+++ PP_Lab2Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab2Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int sum = 0;
+            for (int i=1; i <=n; i++) {
+                sum += i;
+            }
+            Console.WriteLine("Suma " + n + " pierwszych liczb = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
