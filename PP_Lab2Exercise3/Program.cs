3c1e66818f7e832364a868029a5d35104c72c9ae
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-29 18:04:24 +0100

    Added exercise 3 from lab 2

---
+++ PP_Lab2Exercise3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab2Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    number = i * j;
+                    if (number < 10 && j != 10)
+                    {
+                        Console.Write(number + "  ");
+                        if (j == 10)
+                        {
+                            Console.WriteLine(number);
+                        }
+                    }
+                    else
+                    {
+                        if (j != 10)
+                        {
+                            Console.Write(number + " ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(number);
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
