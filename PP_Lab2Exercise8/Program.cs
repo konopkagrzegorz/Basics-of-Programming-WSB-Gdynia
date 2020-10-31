8e43bdd1044445efce076f1149e0c7ec9c5f7cf6
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-31 12:55:00 +0100

    added exercise 8 from lab 2

---
+++ PP_Lab2Exercise8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab2Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many elements in table do you want to have?: ");
+            int elements = Convert.ToInt32(Console.ReadLine());
+            int[] tab = new int[elements];
+
+            for (int i = 0; i < elements; i++)
+            {
+                Console.Write("Give " + (i + 1) + " element: ");
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(tab);
+            Array.Reverse(tab);
+            Console.Write("DESC order sorted table: ");
+            foreach (int value in tab)
+            {
+                Console.Write(value + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
