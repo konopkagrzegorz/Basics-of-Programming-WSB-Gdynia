795596c40866dd4efff7fa176a29f46fb85803e4
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-11-03 20:05:56 +0100

    added exercise 7 in lab 2

---
+++ PP_Lab2Exercise7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab2Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[100];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = i * i;
+            }
+            Console.WriteLine(tab[16]);
+            Console.ReadKey();
+        }
+    }
+}
