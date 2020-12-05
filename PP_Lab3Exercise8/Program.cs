be5765b156a1e94d616ab508835c498ffbc56e16
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-12-05 17:52:29 +0100

    added exercise 8 from 3rd lab

---
+++ PP_Lab3Exercise8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab3Exercise8
+{
+    class Program
+    {
+
+        public static int Obwod(int a)
+        {
+            return 4*a;
+        }
+
+        public static int Obwod(int a, int b)
+        {
+            return 2 * a + 2 * b;
+        }
+
+        public static int Obwod(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Obwod kwadratu o boku 7: " + Obwod(7));
+            Console.WriteLine("Obwod prostokata o bokach 9, 21: " + Obwod(9,21));
+            Console.WriteLine("Obwod trojkata (3,4,5): " + Obwod(3,4,5));
+
+            Console.ReadKey();
+        }
+    }
+}
