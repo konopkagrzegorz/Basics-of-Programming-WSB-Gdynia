7e93df0af3c9d0d0b2c28e22e06bf34f190f2ed8
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-12-05 16:27:23 +0100

    added exercise 7 from 3rd lab

---
+++ PP_Lab3Exercise7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab3Exercise7
+{
+    class Program
+    {
+
+        public static void Brutto(ref double cena)
+        {
+            cena = cena * 1.23;
+        }
+        static void Main(string[] args)
+        {
+            double procesor = 500;
+            double kartaGraficzna = 700;
+            double pamiecRam = 300;
+
+            Brutto(ref procesor);
+            Brutto(ref kartaGraficzna);
+            Brutto(ref pamiecRam);
+
+            Console.WriteLine(procesor + kartaGraficzna + pamiecRam);
+            Console.ReadKey();
+        }
+    }
+}
