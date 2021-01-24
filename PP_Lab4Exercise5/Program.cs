f79ac63c2d0fde0c4368dcd97b244efad90dc7ed
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2021-01-24 08:30:21 +0100

    added exercise 5

---
+++ PP_Lab4Exercise5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab4Exercise5
+{
+    class Program
+    {
+
+        public static int silnia(int n)
+        {
+            if (n < 2)
+            {
+                return 1;
+            }
+            return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Silnia z 25: " + silnia(5));
+            Console.ReadKey();
+
+        }
+    }
+}
