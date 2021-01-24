9ef6989acb7f765e682c1afacfa7bcf84f077a27
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2021-01-24 09:39:57 +0100

    added exercise 2 in lab 5

---
+++ PP_Lab5Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PP_Lab5Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\wiersz.txt";
+            StreamReader sr = new StreamReader(path);
+            string line = "";
+            while (line != null)
+            {
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+            sr.Close();      
+        }
+    }
+}
---
+++ PP_Lab5Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PP_Lab5Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "E:\\kwadraty.txt";
+            StreamWriter sw = new StreamWriter(line);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sw.WriteLine("kwadrat " + i + " to: " + (i * i));
+            }
+            sw.Close();
+        }
+    }
+}
